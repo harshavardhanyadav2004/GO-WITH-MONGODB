190b530f7fc5edfd7933167127cf1446d15ae8e6
Boora sri Harsha Vardhan <138381780+harshavardhanyadav2004@users.noreply.github.com>
2024-06-14 18:56:53 +0530

    Update third.go

--- new_data/third.go
+++ new_data/third.go
@@ -1,1 +1,22 @@
+package main 
+import (
+	"fmt"
+	"mongo_package"
+)
+func main(){
+	 fmt.Println("Connecting into the database")
+	 fmt.Println("Creating  a collection into the database")
+	 mongo_package.ConnectToMongoDB()
+	 fmt.Println("Connecting into  the collection ")
+	 mongo_package.InsertedIntoCollection()
+	 fmt.Println("Retreiving from the database using the MongoDriver")
+	 mongo_package.SelectIntoDataBase()
+	 fmt.Println("Updating in the database using the MongoDriver")
+	 mongo_package.UpdateIntoTheDatabase()
+	 fmt.Println("Deleting from the database")
+	 mongo_package.DeleteFromDatabase()
+	 
+}
+
+
 
